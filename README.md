8440cbc6a62537d15b34c7a071a2370a188f05bd
FoxerLee <foxerlee1@gmail.com>
2018-05-14 18:43:53 +0800

    v0.3.1

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ A pure hand-painted game — 《CrazyGaming》
 ### TODO
 
 - [x] 基础版本
+- [ ] 起始界面修改（doing）
+- [ ] 人物模型修改
 - [ ] 模型、素材更换
-- [ ] 人物模型简化
 - [ ] 关卡设计
 
 ### Thanks
---
+++ js/block.js
@@ -0,0 +1,252 @@
+var tween = (function() {
+
+    function tweenTo(_object, _element, _duration, _repeat) {
+        var count = 0;
+        var ele = {};
+        var org = {};
+        var diff = _element.x - _object.x;
+        for (ob in _element) {
+            ele[ob] = (_element[ob] - _object[ob]) / _duration;
+            org[ob] = (_element[ob] - _object[ob]);
+        }
+
+        requestAnimationFrame(tween);
+
+        function tween() {
+            var cont = false;
+            for (ob in ele) {
+                var test = (ele[ob] > 0 && _object[ob] < _element[ob]);
+                if ((ele[ob] > 0 && _object[ob] < _element[ob]) || (ele[ob] < 0 && _object[ob] > _element[ob])) {
+                    _object[ob] += ele[ob];
+                    cont = true;
+                }
+
+            }
+            if (!cont && _repeat > count) {
+                for (ob in ele) {
+                    org[ob] *= -1;
+                    _element[ob] += org[ob];
+                    ele[ob] *= -1;
+                }
+                cont = true;
+                count++;
+            }
+
+            if (cont) {
+                requestAnimationFrame(tween);
+            }
+
+        }
+    }
+
+    return {
+        to: function(_object, _element, _duration, _repeat) {
+            tweenTo(_object, _element, _duration, _repeat);
+        }
+    };
+})();
+
+$.blocks = {
+    0: {
+        id: 0,
+        name: "bounce"
+    },
+    1: {
+        id: 1,
+        name: "stick"
+    },
+    2: {
+        id: 2,
+        name: "ice"
+    },
+    3: {
+        id: 3,
+        name: "sliderBounce"
+    },
+    4: {
+        id: 4,
+        name: "finish"
+    },
+    8: {
+        id: 8,
+        name: "stopper"
+    },
+    9: {
+        id: 9,
+        name: "theWall"
+    },
+    10: {
+        id: 10,
+        name: "left wall stick"
+    },
+    11: {
+        id: 11,
+        name: "right wall stick"
+    },
+    12: {
+        id: 12,
+        name: "left bounce"
+    },
+    13: {
+        id: 13,
+        name: "right bounce"
+    }
+};
+
+$.blockBehaviors = [{
+    style: "base tramp",
+    ontouch: function() {
+        if ($.myplayer.velocityY <= 0) {
+            $.myplayer.velocityY = 30;
+            $.myplayer.addAnim('bounce');
+            $.playSound($.sounds['bounce1']);
+        }
+    }
+},
+    {
+    w: 800,
+    h: 200,
+    ontouch: function() {
+        if ($.key.right || $.key.left) {
+            $.myplayer.velocityY = .5;
+            $.myplayer.velocityX *= 1.02;
+            $.myplayer.addAnim('grab');
+        }
+    }
+},
+    {
+    style: "base ice",
+    ontouch: function() {
+        if ($.myplayer.velocityY !== 0) {
+            $.playSound($.sounds['bounce3']);
+            $.myplayer.addAnim('flatten');
+
+        };
+        if ($.myplayer.velocityY <= 0 && $.myplayer.y + ($.myplayer.h / 2) < this.y) {
+
+            $.myplayer.velocityX *= .99;
+            $.myplayer.velocityY = $.myplayer.weight;
+            $.myplayer.y = this.y - $.myplayer.h;
+        } else {
+            $.myplayer.velocityY = -10;
+        }
+    }
+},
+    {
+    style: "base tramp",
+    init: function() {
+        tween.to(this, {
+            x: this.x + 100
+        }, 90, 1000);
+    },
+    ontouch: function() {
+        if ($.myplayer.velocityY <= 0) {
+            $.myplayer.velocityY = 30;
+            $.playSound($.sounds['bounce1']);
+            $.myplayer.addAnim('bounce');
+        }
+    }
+},
+    {
+    style: "cube finish",
+    w: 200,
+    h: 50,
+    ontouch: function() {
+        if ($.myplayer.weight > 0) {
+            $.myplayer.success();
+            $.playSound($.sounds['bounce']);
+            $.playSound($.sounds['bounce1']);
+            $.playSound($.sounds['bounce2']);
+            $.playSound($.sounds['bounce3']);
+            $.key.space = 0;
+            $.myplayer.velocityY = 0;
+            $.myplayer.velocityX = 0;
+            $.myplayer.weight = 0;
+        }
+    }
+}, {}, {}, {},
+    {
+    style: "base floor",
+    ontouch: function() {
+        if ($.myplayer.velocityY !== 0) {
+            $.myplayer.addAnim('flatten');
+            $.playSound($.sounds['bounce2']);
+
+        }
+        if ($.myplayer.velocityY <= 0 && $.myplayer.y + ($.myplayer.h / 2) < this.y) {
+
+            $.myplayer.velocityX *= .9;
+
+            $.myplayer.velocityY = $.myplayer.weight;
+            $.myplayer.y = this.y - $.myplayer.h;
+        } else {
+            $.myplayer.velocityY = -10;
+        }
+    }
+},
+    {
+    style: "base wall",
+    add: function() {
+        $.myplayer.velocityY = 0;
+    },
+    w: 100,
+    h: 2100,
+    ontouch: function() {
+        if ($.myplayer.x < this.x) {
+            $.myplayer.velocityX = -1;
+        } else {
+            $.myplayer.velocityX = 1;
+        }
+        $.playSound($.sounds['bounce4']);
+    }
+},
+    {
+    w: 20,
+    h: 200,
+    add: function() {
+        $.myplayer.velocityX += 10;
+    },
+    ontouch: function() {
+        $.myplayer.addAnim('grabL');
+        $.myplayer.velocityY = -1;
+        $.myplayer.velocityX = 0;
+        $.myplayer.x = this.x + this.w;
+    }
+},
+    {
+    w: 20,
+    h: 200,
+    add: function() {
+        $.myplayer.velocityX -= 10;
+    },
+    ontouch: function() {
+        $.myplayer.addAnim('grab');
+        $.myplayer.velocityY = -1;
+        $.myplayer.velocityX = 0;
+        $.myplayer.x = this.x - $.myplayer.w;
+    }
+},
+    {
+        w: 40,
+        h: 200,
+        style: "base Ltramp",
+        ontouch: function() {
+            $.myplayer.velocityX = 30;
+            $.myplayer.velocityY += 10;
+            $.playSound($.sounds['bounce2']);
+            $.myplayer.addAnim('bounce');
+        }
+
+    }, {
+        w: 40,
+        h: 200,
+        style: "base Rtramp",
+        ontouch: function() {
+            $.myplayer.velocityX = -30;
+            $.myplayer.velocityY += 10;
+            $.playSound($.sounds['bounce2']);
+            $.myplayer.addAnim('bounce');
+        }
+
+    }
+];
